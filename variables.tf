7f9a1bbac8ea56a0923af2551d90e6f0c044410a
Patrick Ostie <patrick.ostie@stroeer.de>
2022-05-10 13:13:30 +0200

    refactor: remove runtime var and introduce a type var instead

--- variables.tf
+++ variables.tf
@@ -39,19 +39,12 @@ variable "timeout" {
   default     = 3
 }
 
-variable "runtime" {
-  type        = string
-  description = "The runtime your function should use."
-  default     = "go1.x"
-
-  validation {
-    condition = contains(["nodejs", "nodejs4.3", "nodejs4.3-edge", "nodejs6.10", "nodejs8.10", "nodejs10.x", "nodejs12.x", "nodejs14.x",
+/*
+condition = contains(["nodejs", "nodejs4.3", "nodejs4.3-edge", "nodejs6.10", "nodejs8.10", "nodejs10.x", "nodejs12.x", "nodejs14.x",
       "python2.7", "python3.6", "python3.7", "python3.8", "python3.9", "ruby2.5", "ruby2.7",
       "java8", "java8.al2", "java11", "dotnetcore1.0", "dotnetcore2.0", "dotnetcore2.1", "dotnetcore3.1", "dotnet6",
     "go1.x", "provided", "provided.al2"], var.runtime)
-    error_message = "Make sure to reference a valid runtime. Supported runtimes can be found here: (https://docs.aws.amazon.com/lambda/latest/dg/API_CreateFunction.html#SSS-CreateFunction-request-Runtime)."
-  }
-}
+*/
 
 variable "environment_vars" {
   type        = map(string)
@@ -93,4 +86,15 @@ variable "init_empty" {
   type        = bool
   description = "Controls wether the module should init the lambda with an empty archive file. This comes handy when you want to seperate infrastructure changes from application changes in your workflow."
   default     = false
+}
+
+variable "type" {
+  type = string
+  description = "The type of function you are deploying."
+  default = "go"
+
+  validation {
+    condition = contains(["go", "node"], var.type)
+    error_message = "Currently only go and node functions are supported."
+  }
 }
